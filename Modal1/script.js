55f4cc1d6ffe0889e5e8b6259af76700f257b4c8
Skullkidd12 <araujo.skd@gmail.com>
2022-07-30 01:50:50 -0300

    fiz o projeto de accordion e adicionei filter no modal1

--- Modal1/script.js
+++ Modal1/script.js
@@ -1,17 +1,21 @@
 let openBtn = document.querySelector("#open-btn");
 let modalContainer = document.querySelector("#modal-container");
 let closeBtn = document.querySelector("#close-btn");
+let img = document.querySelector(".img");
 
 openBtn.addEventListener("click", function () {
   modalContainer.style.display = "block";
+  img.style.filter = "Grayscale(50%) ";
 });
 
 closeBtn.addEventListener("click", function () {
   modalContainer.style.display = "none";
+  img.style.filter = "none";
 });
 
 window.addEventListener("click", function (e) {
   if (e.target === modalContainer) {
     modalContainer.style.display = "none";
+    img.style.filter = "none";
   }
 });
