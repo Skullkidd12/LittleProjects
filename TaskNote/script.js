3390b6da8e7f2087b759b2c2fb4e2a2e90c9aa68
Renan Araujo Caetano <araujo.skd@gmail.com>
2022-09-24 01:44:05 -0300

    TaskNote completo, mas as vezes buga

---
+++ TaskNote/script.js
@@ -0,0 +1,83 @@
+//variaveis
+
+const addTask = document.querySelector("#add-task");
+const taskContainer = document.querySelector("#task-container");
+const inputTask = document.querySelector("#input-task");
+
+// event listener
+
+function addition() {
+  let task = document.createElement("div");
+  task.classList.add("task");
+
+  let li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = `<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z"
+    fill="currentColor"
+  />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z"
+    fill="currentColor"
+  />
+</svg>`;
+  checkButton.classList.add("checkTask");
+  task.appendChild(checkButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+    fill="currentColor"
+  />
+  <path d="M9 9H11V17H9V9Z" fill="currentColor" />
+  <path d="M13 9H15V17H13V9Z" fill="currentColor" />
+</svg>`;
+  deleteButton.classList.add("deleteTask");
+  task.appendChild(deleteButton);
+
+  if (inputTask.value === "") {
+    alert("Please Enter a Task");
+  } else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+
+  checkButton.addEventListener("click", function () {
+    checkButton.parentElement.style.textDecoration = "line-through";
+    checkButton.parentElement.style.backgroundColor = "rgb(2, 219, 147)";
+    checkButton.parentElement.style.color = "#ffffff";
+    checkButton.style.backgroundColor = "rgb(2, 219, 147)";
+    checkButton.style.color = "#ffffff";
+    deleteButton.style.backgroundColor = "rgb(2, 219, 147)";
+    deleteButton.style.color = "#ffffff";
+  });
+
+  deleteButton.addEventListener("click", function (e) {
+    let target = e.target;
+    target.parentElement.parentElement.remove();
+  });
+}
+
+addTask.addEventListener("click", addition);
